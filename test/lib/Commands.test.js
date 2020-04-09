90484b50eb8708668ae413a4f4eef843a8cd7f9c
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2020-04-09 01:11:48 -0700

    Add command argument string parser

--- test/lib/Commands.test.js
+++ test/lib/Commands.test.js
@@ -24,4 +24,27 @@ describe('Commands',() =>
             });
         });
     });
+
+    describe('extract_command_argument_string',() =>
+    {
+        describe('for a command alone', () =>
+        {
+            it('should return empty string', () =>
+            {
+                const raw_message = '!test';
+
+                expect(Commands.extract_command_argument_string(raw_message)).to.be.equal('');
+            });
+        });
+
+        describe('for a command with arguments', () =>
+        {
+            it('should extract the argument string correctly', () =>
+            {
+                const raw_message = '!test blabla bla';
+
+                expect(Commands.extract_command_argument_string(raw_message)).to.be.equal('blabla bla');
+            });
+        });
+    });
 });
